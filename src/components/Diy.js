fe64f610da93f9123433560cc58fa497e8e0fa03
Addison <addison@yunio.com>
2019-06-29 12:17:10 +0800

    判断localGoods是否为空

--- src/components/Diy.js
+++ src/components/Diy.js
@@ -3,81 +3,84 @@ import { Table, Tag, message } from 'antd';
 
 export default class News extends React.PureComponent {
 
-	state = {
-		current: 'CPU',
-		data: [],
-		template: [
-			{
-				id: '0',
-				name: 'CPU',
-				good: ''
-			}, {
-				id: '1',
-				name: '主板',
-				good: ''
-			}, {
-				id: '2',
-				name: '内存',
-				good: ''
-			}, {
-				id: '3',
-				name: '硬盘',
-				good: ''
-			}, {
-				id: '4',
-				name: '固态',
-				good: ''
-			}, {
-				id: '5',
-				name: '显卡',
-				good: ''
-			}, {
-				id: '6',
-				name: '显示',
-				good: ''
-			}, {
-				id: '7',
-				name: '机箱',
-				good: ''
-			}, {
-				id: '8',
-				name: '电源',
-				good: ''
-			}, {
-				id: '9',
-				name: '散热',
-				good: ''
-			}, {
-				id: '10',
-				name: '鼠标',
-				good: ''
-			}, {
-				id: '11',
-				name: '键盘',
-				good: ''
-			}
+  state = {
+    current: 'CPU',
+    data: [],
+    template: [
+      {
+        id: '0',
+        name: 'CPU',
+        good: ''
+      }, {
+        id: '1',
+        name: '主板',
+        good: ''
+      }, {
+        id: '2',
+        name: '内存',
+        good: ''
+      }, {
+        id: '3',
+        name: '硬盘',
+        good: ''
+      }, {
+        id: '4',
+        name: '固态',
+        good: ''
+      }, {
+        id: '5',
+        name: '显卡',
+        good: ''
+      }, {
+        id: '6',
+        name: '显示',
+        good: ''
+      }, {
+        id: '7',
+        name: '机箱',
+        good: ''
+      }, {
+        id: '8',
+        name: '电源',
+        good: ''
+      }, {
+        id: '9',
+        name: '散热',
+        good: ''
+      }, {
+        id: '10',
+        name: '鼠标',
+        good: ''
+      }, {
+        id: '11',
+        name: '键盘',
+        good: ''
+      }
     ],
     localGoods: [],
     price: 0
-	}
+  }
 
-	componentDidMount() {
+  componentDidMount() {
     const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
     let price = 0;
-    for (const l of localGoodsParse) {
-      price += Number(l.price)
+    if (localGoodsParse) {
+      for (const l of localGoodsParse) {
+        price += Number(l.price)
+      }
+      this.setState({
+        localGoods: localGoodsParse,
+        price
+      })
     }
-    this.setState({
-      localGoods: localGoodsParse,
-      price
-    })
-	}
 
-	handleClick = (info) => {
-		// console.log(info.key)
-		this.setState({
-			current: info.key
-		})
+  }
+
+  handleClick = (info) => {
+    // console.log(info.key)
+    this.setState({
+      current: info.key
+    })
   }
 
   cutGoods = (info) => {
@@ -107,70 +110,71 @@ export default class News extends React.PureComponent {
     }
   }
 
-	render() {
-		const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
+  render() {
+    // const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
+    const { localGoods } = this.state
 
-		const columns = [
-			{
-				title: '类型',
-				dataIndex: 'name',
-				key: 'name',
-				render: text => <a href="">{text}</a>,
-      },
-			{
-				title: '型号',
-				dataIndex: 'title',
-				key: 'title',
+    const columns = [
+      {
+        title: '类型',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a href="">{text}</a>,
       },
       {
-				title: '价格',
-				dataIndex: 'price',
-				key: 'price',
-				render: text => <a href="" style={{ color: '#DC143C', fontWeight: 'bold' }}>￥{text}</a>
-			},
+        title: '型号',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: '价格',
+        dataIndex: 'price',
+        key: 'price',
+        render: text => <a href="" style={{ color: '#DC143C', fontWeight: 'bold' }}>￥{text}</a>
+      },
       {
         title: '图片',
         dataIndex: 'img',
         key: 'img',
         render: text => <img src={`http://127.0.0.1:3000${text}`} style={{ width: 100 }} alt='img' />
       },
-			{
-				title: '标签',
-				key: 'params',
-				dataIndex: 'params',
-				render: tags => (
-					<span>
-						{tags.map(tag => {
-							let color = 'geekblue';
-							if (tag === 'loser') {
-								color = 'volcano';
-							}
-							return (
-								<Tag color={color} key={tag.name}>
-									{`${tag.name}: ${tag.value}`}
-								</Tag>
-							);
-						})}
-					</span>
-				),
-			},
-			{
-				title: '操作',
-				key: 'action',
-				render: (text, record) => (
-					<span>
-						<a onClick={e => this.cutGoods(text)}>移除</a>
-					</span>
-				),
-			},
-		];
+      {
+        title: '标签',
+        key: 'params',
+        dataIndex: 'params',
+        render: tags => (
+          <span>
+            {tags.map(tag => {
+              let color = 'geekblue';
+              if (tag === 'loser') {
+                color = 'volcano';
+              }
+              return (
+                <Tag color={color} key={tag.name}>
+                  {`${tag.name}: ${tag.value}`}
+                </Tag>
+              );
+            })}
+          </span>
+        ),
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a onClick={e => this.cutGoods(text)}>移除</a>
+          </span>
+        ),
+      },
+    ];
 
     const { price } = this.state;
 
-		return (
-			<div>
-				<Table pagination={false} columns={columns} dataSource={localGoodsParse.map((e, index) => {
-					return {
+    return (
+      <div>
+        <Table pagination={false} columns={columns} dataSource={localGoods ? localGoods.map((e, index) => {
+          return {
             key: String(index + 1),
             _id: e._id,
             name: e.type,
@@ -178,10 +182,10 @@ export default class News extends React.PureComponent {
             title: e.name,
             img: e.img,
             params: e.params
-					}
-				})} />
+          }
+        }) : {}} />
         <h3 style={{ fontWeight: 'bold' }}>总价：<span style={{ color: '#DC143C' }}>{price}</span></h3>
-			</div>
-		)
-	}
+      </div>
+    )
+  }
 }
--- src/components/Machine.js
+++ src/components/Machine.js
@@ -4,9 +4,9 @@ import { getGoods } from '../services/xiamiServer';
 
 export default class News extends React.PureComponent {
 
-	state = {
-		current: 'CPU',
-		template: [
+  state = {
+    current: 'CPU',
+    template: [
       {
         id: '0',
         name: 'CPU',
@@ -59,26 +59,26 @@ export default class News extends React.PureComponent {
     ],
     products: [],
     localGoods: []
-	}
+  }
 
-	componentDidMount() {
-		getGoods(this.state.current).then(res => {
+  componentDidMount() {
+    getGoods(this.state.current).then(res => {
       const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
       this.setState({
         products: res.data,
         localGoods: localGoodsParse
       })
-		})
-	}
+    })
+  }
 
-	handleClick = (info) => {
-		// console.log(info.key)
+  handleClick = (info) => {
+    // console.log(info.key)
     getGoods(info.key).then(res => {
       this.setState({
         products: res.data,
         current: info.key
       })
-		})
+    })
   }
 
   cutGoods = (text, record) => {
@@ -107,13 +107,16 @@ export default class News extends React.PureComponent {
     const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
     const localGoods = [];
 
-    for (const e of localGoodsParse) {
-      if (e.type === text.type) {
-        return null;
-      } else {
-        localGoods.push(e);
+    if (localGoodsParse) {
+      for (const e of localGoodsParse) {
+        if (e.type === text.type) {
+          return null;
+        } else {
+          localGoods.push(e);
+        };
       };
-    };
+    }
+
 
     localGoods.push(text);
 
@@ -129,81 +132,83 @@ export default class News extends React.PureComponent {
 
   }
 
-	render() {
+  render() {
     const { current, template } = this.state;
     const goods = [];
 
     const localGoodsParse = JSON.parse(localStorage.getItem('goods'));
-    for (const e of localGoodsParse) {
-      goods.push(e._id)
+    if (localGoodsParse) {
+      for (const e of localGoodsParse) {
+        goods.push(e._id)
+      }
     }
 
-		const columns = [
-			{
-				title: '型号',
-				dataIndex: 'name',
-				key: 'name',
-				render: text => <a href="">{text}</a>,
-			},
-			{
-				title: '价格',
-				dataIndex: 'price',
-				key: 'price',
-				render: text => <a href="" style={{ color: '#DC143C', fontWeight: 'bold' }}>￥{text}</a>
-			},
-			{
-				title: '图片',
-				dataIndex: 'img',
+    const columns = [
+      {
+        title: '型号',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a href="">{text}</a>,
+      },
+      {
+        title: '价格',
+        dataIndex: 'price',
+        key: 'price',
+        render: text => <a href="" style={{ color: '#DC143C', fontWeight: 'bold' }}>￥{text}</a>
+      },
+      {
+        title: '图片',
+        dataIndex: 'img',
         key: 'img',
         render: text => <img src={`http://127.0.0.1:3000${text}`} style={{ width: 100 }} alt='img' />
-			},
-			{
-				title: '标签',
-				key: 'params',
-				dataIndex: 'params',
-				render: tags => (
-					<span>
-						{tags.map(tag => {
-							let color = 'geekblue';
-							if (tag === 'loser') {
-								color = 'volcano';
-							}
-							return (
-								<Tag color={color} key={tag.name}>
-									{`${tag.name}: ${tag.value}`}
-								</Tag>
-							);
-						})}
-					</span>
-				),
-			},
-			{
-				title: '操作',
-				key: 'action',
-				render: (text, record) => (
-					<span>
+      },
+      {
+        title: '标签',
+        key: 'params',
+        dataIndex: 'params',
+        render: tags => (
+          <span>
+            {tags.map(tag => {
+              let color = 'geekblue';
+              if (tag === 'loser') {
+                color = 'volcano';
+              }
+              return (
+                <Tag color={color} key={tag.name}>
+                  {`${tag.name}: ${tag.value}`}
+                </Tag>
+              );
+            })}
+          </span>
+        ),
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+          <span>
             {
               goods.indexOf(text._id) !== -1 ?
-              <a onClick={e => this.cutGoods(text, record)}>{goods.indexOf(text._id) !== -1 ? '取消' : ''}</a> :
-              <a onClick={e => this.addGoods(text, record)}>{goods.indexOf(text._id) !== -1 ? '' : '添加'}</a>
+                <a onClick={e => this.cutGoods(text, record)}>{goods.indexOf(text._id) !== -1 ? '取消' : ''}</a> :
+                <a onClick={e => this.addGoods(text, record)}>{goods.indexOf(text._id) !== -1 ? '' : '添加'}</a>
             }
 
-					</span>
-				),
-			},
+          </span>
+        ),
+      },
     ];
 
     const { products } = this.state;
 
-		return (
-			<div>
-				<Card style={{ minHeight: 970 }}>
-					<Menu onClick={this.handleClick} selectedKeys={[current]} mode="inline" style={{ width: 100, height: 0 }}>
-						{template.map((e) => <Menu.Item key={e.name}>{e.name}</Menu.Item>)}
-					</Menu>
-					<Table style={{ width: '85%', marginLeft: 150 }} columns={columns} dataSource={products} />
-				</Card>
-			</div>
-		)
-	}
+    return (
+      <div>
+        <Card style={{ minHeight: 970 }}>
+          <Menu onClick={this.handleClick} selectedKeys={[current]} mode="inline" style={{ width: 100, height: 0 }}>
+            {template.map((e) => <Menu.Item key={e.name}>{e.name}</Menu.Item>)}
+          </Menu>
+          <Table style={{ width: '85%', marginLeft: 150 }} columns={columns} dataSource={products ? products : {}} />
+        </Card>
+      </div>
+    )
+  }
 }
